5614755ac95fdc934990b46ee739204463cf5d95
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-18 20:29:41 +0600

    Remove null value

--- db/migrate/20230818114358_add_foreignkey_posts.rb
+++ db/migrate/20230818114358_add_foreignkey_posts.rb
@@ -1,5 +1,5 @@
 class AddForeignkeyPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :author, null: false, foreign_key: { to_table: :users , column: :user_id}
+    add_reference :posts, :author, foreign_key: { to_table: :users , column: :user_id}
   end
 end
--- db/migrate/20230818114851_add_foreignkey_comments.rb
+++ db/migrate/20230818114851_add_foreignkey_comments.rb
@@ -1,6 +1,6 @@
 class AddForeignkeyComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :author, null: false, foreign_key: { to_table: :users , column: :user_id}
-    add_reference :comments, :post, null: false, foreign_key: { to_table: :users , column: :post_id}
+    add_reference :comments, :author, foreign_key: { to_table: :users , column: :user_id}
+    add_reference :comments, :post, foreign_key: { to_table: :users , column: :post_id}
   end
 end
