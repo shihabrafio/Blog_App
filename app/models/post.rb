c744838785692e442e4d3bca303ce9966e2060c6
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-08-18 20:28:48 +0600

    Add author counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,18 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  
+  
+  def recent_posts
+    posts.order(created_at: :desc).limit(5)
+  end
+  
+  after_save :update_user_post_counter
+  
+  private
+
+  def update_user_post_counter
+    author.increment!(:posts_counter)
+  end
+
 end
